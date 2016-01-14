67bf31ead3b159735ac44d58981052aefd1ce737
23RDGit <Jackson.GAringer@outlook.com>
2016-01-14 17:23:21 -0600

    Initial Commit

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Jackson Garinger
+
+public class PlayerMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("w")) // Move Forward
+            transform.Translate(0.0f, 1.0f, 0.0f);
+
+        if (Input.GetKeyDown("s")) // Move Backward
+            transform.Translate(0.0f, -1.0f, 0.0f);
+
+        if (Input.GetKeyDown("a")) // Move Left
+            transform.Translate(-1.0f, 0.0f, 0.0f);
+
+        if (Input.GetKeyDown("d")) // Move Right
+            transform.Translate(1.0f, 0.0f, 0.0f);
+
+        if (Input.GetKeyDown("q")) // Move Forward-Left Diagonal
+            transform.Translate(-1.0f, 1.0f, 0.0f);
+
+        if (Input.GetKeyDown("e")) // Move Forward-Right Diagonal
+            transform.Translate(1.0f, 1.0f, 0.0f);
+
+        if (Input.GetKeyDown("z")) // Move Backward-Left Diagonal
+            transform.Translate(-1.0f, -1.0f, 0.0f);
+
+        if (Input.GetKeyDown("c")) // Move Backward-Right Diagonal
+            transform.Translate(1.0f, -1.0f, 0.0f);
+
+    }
+}
