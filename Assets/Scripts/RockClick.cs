6e30792f3ed709bf3f88446fb97f1092775baa54
barsh <michael.barshinger@gmail.com>
2016-01-16 09:41:30 -0600

    player movement

---
+++ Assets/Scripts/RockClick.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockClick : MonoBehaviour {
+
+	void OnMouseDown() {
+		var player = GameObject.FindGameObjectWithTag ("player");
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
